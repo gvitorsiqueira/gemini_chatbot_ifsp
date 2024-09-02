064b912834534a2088cd5f0e26d552342562157f
gvsiqueira <guilherme.vitor.siqueira@gmail.com>
2024-09-02 15:28:39 -0300

    updated chroma db

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -5,5 +5,5 @@ import google.generativeai as genai
 import streamlit as st
 google_api_key=st.secrets["GOOGLE_API_KEY"]
 def get_embedding_function():
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-004", google_api_key=google_api_key)
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004", google_api_key=google_api_key)
     return embeddings
